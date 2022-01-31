95cc5d52dd25b25db826f793db91b650a23a8e27
teja-MXX <teja.mandapakala@gmail.com>
2022-01-31 22:13:17 +0530

    Added Interfaces and retreive IP from interface

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import platform
-import subprocess
+#! /usr/bin/env python
+import netifaces as ni
 import socket
+from time import sleep
 from colorama import init , Fore, Back
 from ping3 import ping
+import scapy.all as scapy
 init()
 
-def icmp(host):
+online = {}
+
+def pings(host):
     resp = ping(host)
     if resp:
         print("Online")
+        online[host] = {}
     else:
         print("Unreachable")
 
@@ -22,14 +27,29 @@ __________                  __             __         __      __          __    
 
 \033[0m""")
 
-NetworkAddress = socket.gethostbyname(socket.gethostname())
-Last_Octet = NetworkAddress.rfind(".")
-NetworkAddress = NetworkAddress[:10]
-print("Network Address - ",NetworkAddress+"0")
-print("Pinging 25 IP addresses in the above detected Network \n")
+print("* Looking for Interfaces")
+sleep(3)
+interfaces = scapy.get_if_list()
+interfaceCount = len(interfaces)
+print("* Found "+str(interfaceCount)+" interfaces")
+sleep(3)
+print("* Choose the network interface of your choice -")
+for x in range(interfaceCount):
+    print(f"     {x+1} . {interfaces[x]} ")
+interfaceChoice = int(input("\n* Choice - "))-1
+sleep(1.5)
+interface = interfaces[interfaceChoice]
+print("* Getting interface IP - ",end="")
+sleep(0.5)
+interfaceIP = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+print(interfaceIP)
+Last_Octet = interfaceIP.rfind(".")
+NetworkAddress = interfaceIP[:10]
+print("* Network Address - ",NetworkAddress+"0")
+sleep(1)
+print("* Pinging 25 IP addresses in the above detected Network \n")
 for x in range(1,26,1):
     IP = NetworkAddress+str(x)
-    print("Pinging "+IP+" -- ",end=" ")
-    icmp(IP)
-
+    print("  Pinging "+IP+" -- ",end=" ")
+    pings(IP)
 
